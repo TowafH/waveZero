81348fb039dc97139335f5467d3dabeaf85d16a3
TowafH <towafh@nycstudents.net>
2024-08-10 19:19:13 -0400

    Added API to Track Temp/Location/Time/Description

--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -2,10 +2,22 @@
 let locationInput = document.getElementById("locationInput");
 let submitBtn = document.getElementById("submitBtn");
 
+let locationTemp = document.getElementById("locationTemp");
+let locationTitle = document.getElementById("locationTitle");
+let locationTime = document.getElementById("locationTime")
+let weatherDescription = document.getElementById("weatherDesc")
+
+let dayBoxes = document.getElementById("dayBoxes");
+let dayHeading = document.getElementById("dayHeading");
+let dayTemp = document.querySelectorAll(".dayTemp");
+let dayDate = document.querySelectorAll(".dayDate");
+let dayDesc = document.querySelectorAll(".dayDesc");
 //Functions
 
+let restrictDaysView = 3;
+
 function generateWeather(event){
-    
+
 //Prevent Page Refresh
 event.preventDefault();
 
@@ -21,9 +33,47 @@ fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/service
         return response.json();
     })
     .then(function(myWeatherData){
-        console.log(myWeatherData)
+        //Preview JSON in Console
+        console.log(myWeatherData);
+
+        //Location Temp
+        locationTemp.innerText = myWeatherData.currentConditions.temp + " °F";
+
+        //Location Name
+        locationTitle.innerText = myWeatherData.resolvedAddress;
+
+        //Location Time
+        locationTime.innerText = new Date(myWeatherData.currentConditions.datetimeEpoch * 1000).toLocaleString('en-US', {
+            hour: 'numeric',   
+            minute: 'numeric', 
+            hour12: true       // 12-hour time format
+        });
+
+        //Weather Description
+        weatherDescription.innerText = myWeatherData.description;
+        
+        //Display Boxes+Heading
+        dayHeading.style.display = "block";
+        dayHeading.style.display = "flex";
+        dayBoxes.style.display = "block";
+        dayBoxes.style.display = "flex";
+
+        //Select Date for the three days
+        for (let i = 0; i < restrictDaysView; i++){
+            dayDate[i].innerText = myWeatherData.days[i].datetime;
+        }
+
+        //Select Temps for the three days
+        for (let i = 0; i < restrictDaysView; i++){
+            dayTemp[i].innerText = myWeatherData.days[i].temp + " °F";
+        }
+
+        //Select Desc for the three days
+        for(let i = 0; i < restrictDaysView; i++){
+            dayDesc[i].innerText = myWeatherData.days[i].description;
+        }
     })
-//Check for Invalid Inputs
+    //Check for Invalid Inputs
     .catch(function(error) {
         alert('Failed to fetch weather data. Please try again!');
     })
