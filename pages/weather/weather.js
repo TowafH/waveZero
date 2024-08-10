19784f9a06476f391862c2d219fd100a34ebf2f1
TowafH <towafh@nycstudents.net>
2024-08-10 16:56:19 -0400

    Update

--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -5,6 +5,7 @@ let submitBtn = document.getElementById("submitBtn");
 //Functions
 
 function generateWeather(event){
+    
 //Prevent Page Refresh
 event.preventDefault();
 
@@ -15,18 +16,17 @@ console.log(userInputCountry);
 //Fetch Data
 fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + userInputCountry + "?unitGroup=us&key=49D2YD468V5AG8DTSDS8DRWZF&contentType=json")
 //Preview the JSON 
-.then(function(response){
-    console.log(response);
-    return response.json();
-})
-.then(function(myWeatherData){
-    //View the returned response.json(); in the Console with this parameter+console.log
-    console.log(myWeatherData)
-})
-.catch(function(error) {
-    alert('Failed to fetch weather data. Please try again!');
-})
-
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    })
+    .then(function(myWeatherData){
+        console.log(myWeatherData)
+    })
+//Check for Invalid Inputs
+    .catch(function(error) {
+        alert('Failed to fetch weather data. Please try again!');
+    })
 }
 
 
