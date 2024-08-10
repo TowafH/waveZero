8a1f98212c2225be4383a3e2a24ff1e7b4de7c81
TowafH <towafh@nycstudents.net>
2024-08-09 23:25:36 -0400

    Added API+UserInput

--- pages/weather/weather.js
+++ pages/weather/weather.js
@@ -1,5 +1,20 @@
-fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/United%20States?unitGroup=us&key=49D2YD468V5AG8DTSDS8DRWZF&contentType=json")
- //Preview the JSON 
+//Select Elements
+let locationInput = document.getElementById("locationInput");
+let submitBtn = document.getElementById("submitBtn");
+
+//Functions
+
+function generateWeather(event){
+//Prevent Page Refresh
+event.preventDefault();
+
+//Include userInput into the input field
+let userInputCountry = locationInput.value;
+console.log(userInputCountry);
+
+//Fetch Data
+fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + userInputCountry + "?unitGroup=us&key=49D2YD468V5AG8DTSDS8DRWZF&contentType=json")
+//Preview the JSON 
 .then(function(response){
     console.log(response);
     return response.json();
@@ -8,3 +23,11 @@ fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/service
     //View the returned response.json(); in the Console with this parameter+console.log
     console.log(myWeatherData)
 })
+.catch(function(error) {
+    alert('Failed to fetch weather data. Please try again!');
+})
+
+}
+
+
+submitBtn.addEventListener("click", generateWeather);
