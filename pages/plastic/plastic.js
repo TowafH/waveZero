1290f5f50e3575d4294d76ee4bf6c85ac872bae9
TowafH <towafh@nycstudents.net>
2024-08-12 17:32:01 -0400

    Remove Google Maps

--- pages/plastic/plastic.js
+++ pages/plastic/plastic.js
@@ -1,42 +1,14 @@
-let map;
-let marker;
-
-function initMap() {
-    let defaultLocation = {
-        lat: 40.712,
-        lng: -74.005
-    };
-
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: defaultLocation
-    });
-
-    marker = new google.maps.Marker({
-        position: defaultLocation,
-        map: map
-    });
+// Fetch microplastic information from JSON and display it
+function getMicroplasticInfo() {
+    fetch('plastics.json')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+        .catch(function() {
+            alert("Failed to Fetch Data!")
+        });
 }
 
-let submitBtn = document.getElementById("submitBtn")
-
-function showMicroPlastics() {
-
-fetch("plastics.json")
-//Preview the JSON 
-.then(function(response){
-    console.log(response);
-    return response.json();
-})
-.then(function(myPlasticData){
-    //Preview JSON in Console
-    console.log(myPlasticData);
-
-})
-//Check for Invalid Inputs
-.catch(function(error) {
-    alert('Failed to fetch weather data. Please try again!');
-})
-}
-
-submitBtn.addEventListener("click", showMicroPlastics);
